4f36c47798c35edb5b2c23656f6eb135c280b7a9
Yvan Menuet <74631211+ymwizeline@users.noreply.github.com>
2021-01-18 11:25:44 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import PokeCard from "./components/PokeCard";
+
+export default function App() {
+  return (
+    <div className="App">
+      <PokeCard />
+    </div>
+  );
+}
